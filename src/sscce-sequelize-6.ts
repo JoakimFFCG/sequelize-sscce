322d16e1e33a4111e9bbcb97a2983f7d9c863d2e
Joakim Falk <joakim.falk@ffcg.se>
2022-11-16 20:45:16 +0100

    Test cases

--- src/sscce-sequelize-6.ts
+++ src/sscce-sequelize-6.ts
@@ -1,4 +1,4 @@
-import { DataTypes, Model } from 'sequelize';
+import { DataTypes, Model, NonAttribute } from 'sequelize';
 import { createSequelize6Instance } from '../setup/create-sequelize-instance';
 import { expect } from 'chai';
 import sinon from 'sinon';
@@ -21,10 +21,26 @@ export async function run() {
     },
   });
 
-  class Foo extends Model {}
+  class Foo extends Model {
+    declare id: number;
+    declare name: string;
+    declare note: string;
+    declare setNote: NonAttribute<string>;
+  }
 
   Foo.init({
     name: DataTypes.TEXT,
+    note: {
+      type: DataTypes.TEXT,
+      defaultValue: '',
+    },
+    setNote: {
+      type: new DataTypes.VIRTUAL(DataTypes.STRING, ['name']),
+      set (this: Foo, value) {
+        const name = this.getDataValue('name');
+        this.setDataValue('note', `${name}: ${value}`);
+      }
+    }
   }, {
     sequelize,
     modelName: 'Foo',
@@ -36,6 +52,41 @@ export async function run() {
   await sequelize.sync({ force: true });
   expect(spy).to.have.been.called;
 
-  console.log(await Foo.create({ name: 'TS foo' }));
-  expect(await Foo.count()).to.equal(1);
+  let foo: Foo | null = await Foo.create({ name: 'Name' });
+  const id = foo.id;
+  expect(foo.name).to.equal('Name'); // Works
+
+  foo.setNote = 'Note';
+  foo = await foo.save();
+  expect(foo.note).to.equal('Name: Note'); // Works
+
+  foo.name = 'Name2';
+  foo.setNote = 'Note2';
+  foo = await foo.save();
+  expect(foo.note).to.equal('Name2: Note2'); // Works
+
+  foo.setNote = 'Note3';
+  foo.name = 'Name3';
+  foo = await foo.save();
+  expect(foo.note).to.equal('Name3: Note3'); // Doesn't work
+
+  await Foo.update({
+    name: 'Name4',
+    setNote: 'Note4',
+  }, { where: { id } });
+  foo = await Foo.findByPk(id);
+  expect(foo?.note).to.equal('Name4: Note4'); // Works
+
+  await Foo.update({
+    setNote: 'Note5',
+  }, { where: { id } });
+  foo = await Foo.findByPk(id);
+  expect(foo?.note).to.equal('Name4: Note5'); // Doesn't work
+
+  await Foo.update({
+    setNote: 'Note6',
+    name: 'Name6',
+  }, { where: { id } });
+  foo = await Foo.findByPk(id);
+  expect(foo?.note).to.equal('Name6: Note6'); // Doesn't work
 }
